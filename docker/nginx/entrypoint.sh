345c50db82d9b952c6dbb28e1c4a7b0eba6a5015
mohammad <mohammadtbz76@gmail.com>
2026-07-06 11:23:24 +0330

    [ADD] fix docker

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -5,10 +5,10 @@ echo "Starting Nginx"
 
 if [ "$SSL_ENABLED" = "true" ]; then
     echo "SSL enabled: loading HTTPS config"
-    cp /etc/nginx/templates/https.conf /etc/nginx/conf.d/default.conf
+    cp /etc/nginx/https.conf /etc/nginx/conf.d/default.conf
 else
     echo "SSL disabled: loading HTTP config"
-    cp /etc/nginx/templates/http.conf /etc/nginx/conf.d/default.conf
+    cp /etc/nginx/http.conf /etc/nginx/conf.d/default.conf
 fi
 
 nginx -g 'daemon off;'
